e426641d3d2832d4c012291f56603d8641a20109
Meshith Ariyawansa <71543277+Meshydra@users.noreply.github.com>
2023-02-19 00:47:21 +0530

    Final
    Project finished.
    Bugs noticed: Can not resize. scaling issues.

--- Notepad/src/main/java/texteditor/textEditorGUI.java
+++ Notepad/src/main/java/texteditor/textEditorGUI.java
@@ -2,22 +2,26 @@ package texteditor;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FileDialog;
 import java.awt.Image;
+import java.awt.Shape;
+import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollBar;
-import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 public class textEditorGUI extends javax.swing.JFrame {
@@ -28,9 +32,20 @@ public class textEditorGUI extends javax.swing.JFrame {
     ImageIcon imageLogoh = scaleImage("resources/logoh.png", 32);
     ImageIcon imageLogo = scaleImage("resources/logo.png", 32);
     
+    ImageIcon imageCloseh = scaleImage("resources/closeh.png", 24);
+    ImageIcon imageClose = scaleImage("resources/close.png", 24);
+    
+    int xMouse;
+    int yMouse;
+    
     
     public textEditorGUI() {
         initComponents();
+        setIconImage(Toolkit.getDefaultToolkit().getImage("resources/logo.png"));
+         Shape roundedRectangle = new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 20, 20);
+
+        // set the shape of the frame
+        setShape(roundedRectangle);
         
     }
     @SuppressWarnings("unchecked")
@@ -46,12 +61,14 @@ public class textEditorGUI extends javax.swing.JFrame {
         itemCut1 = new javax.swing.JMenuItem();
         itemCopy1 = new javax.swing.JMenuItem();
         itemPaste1 = new javax.swing.JMenuItem();
+        jPanel3 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         textArea = new javax.swing.JTextArea();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel()
         ;
+        btnClose = new javax.swing.JLabel();
 
         mainMenu.setBackground(new java.awt.Color(44, 47, 51));
         mainMenu.setForeground(new java.awt.Color(255, 255, 255));
@@ -138,9 +155,21 @@ public class textEditorGUI extends javax.swing.JFrame {
         });
         subMenu.add(itemPaste1);
 
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(35, 39, 42));
-        setMinimumSize(new java.awt.Dimension(500, 500));
+        setUndecorated(true);
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(44, 47, 51));
 
@@ -174,14 +203,14 @@ public class textEditorGUI extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 595, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 463, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -202,8 +231,19 @@ public class textEditorGUI extends javax.swing.JFrame {
         verticalScrollBar.setBackground(new Color(54,57,65));
 
         jPanel2.setBackground(new java.awt.Color(44, 47, 51));
+        jPanel2.setCursor(new java.awt.Cursor(java.awt.Cursor.MOVE_CURSOR));
+        jPanel2.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jPanel2MouseDragged(evt);
+            }
+        });
+        jPanel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jPanel2MousePressed(evt);
+            }
+        });
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 1, 14)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Segoe Print", 0, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setIcon(imageLogo);
         jLabel1.setText("Scribble");
@@ -227,19 +267,45 @@ public class textEditorGUI extends javax.swing.JFrame {
             }
         });
 
+        btnClose.setText("jLabel2");
+        btnClose.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnClose.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnCloseMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnCloseMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnCloseMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btnCloseMousePressed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(15, 15, 15)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(490, Short.MAX_VALUE))
+                .addGap(17, 17, 17)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnClose, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnClose, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        btnClose.setIcon(imageClose);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -251,7 +317,7 @@ public class textEditorGUI extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -333,6 +399,39 @@ public class textEditorGUI extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jLabel1MouseReleased
 
+    private void jPanel2MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel2MouseDragged
+        // TODO add your handling code here:
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+
+        this.setLocation(x- xMouse,y-yMouse);
+    }//GEN-LAST:event_jPanel2MouseDragged
+
+    private void jPanel2MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel2MousePressed
+        // TODO add your handling code here:
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }//GEN-LAST:event_jPanel2MousePressed
+
+    private void btnCloseMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCloseMouseEntered
+        // TODO add your handling code here:
+        btnClose.setIcon(imageCloseh);
+    }//GEN-LAST:event_btnCloseMouseEntered
+
+    private void btnCloseMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCloseMouseExited
+        // TODO add your handling code here:
+        btnClose.setIcon(imageClose);
+    }//GEN-LAST:event_btnCloseMouseExited
+
+    private void btnCloseMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCloseMousePressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnCloseMousePressed
+
+    private void btnCloseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCloseMouseClicked
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_btnCloseMouseClicked
+
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -340,52 +439,48 @@ public class textEditorGUI extends javax.swing.JFrame {
             }
         });
     }
-    
-     public void openFile(){
-        FileDialog fileDialog = new FileDialog(textEditorGUI.this, "Open File", FileDialog.LOAD);
-        fileDialog.setVisible(true);
-        
-        if(fileDialog.getFile() != null){
-            filename = fileDialog.getDirectory() + fileDialog.getFile();
-            setTitle(filename);
+    public void openFile() {
+    JFileChooser fileChooser = new JFileChooser();
+    fileChooser.setFileFilter(new FileNameExtensionFilter("Text documents (*.txt)", "txt"));
+    int result = fileChooser.showOpenDialog(textEditorGUI.this);
+    if (result == JFileChooser.APPROVE_OPTION) {
+        String filename = fileChooser.getSelectedFile().getPath();
+        try {
+            String fileContents = Files.readString(Paths.get(filename));
+            textArea.setText(fileContents);
+        } catch (IOException e) {
+            String errorMessage = "An error occurred while reading the file.";
+            JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+            System.out.print(errorMessage);
         }
-        try{
-            BufferedReader reader = new  BufferedReader(new FileReader(filename));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            line = null;
-            while((line = reader.readLine())!=null){
-                sb.append(line).append("\n");
-                textArea.setText(sb.toString());
-             }
-            reader.close();
-        }catch(IOException e){
-            System.out.println("File Not Found");
-        }
+    }
 }
      
      public void newFile(){
      textArea.setText("");
         setTitle(filename);
 }
-     public void saveFile(){
-         FileDialog fileDialog = new FileDialog(textEditorGUI.this, "Save File", FileDialog.SAVE);
-        fileDialog.setVisible(true);
-        if(fileDialog.getFile() != null){
-            filename = fileDialog.getDirectory() + fileDialog.getFile();
-            setTitle(filename);
+     public void saveFile() {
+    JFileChooser fileChooser = new JFileChooser();
+    fileChooser.setFileFilter(new FileNameExtensionFilter("Text documents (*.txt)", "txt"));
+    int result = fileChooser.showSaveDialog(textEditorGUI.this);
+    if (result == JFileChooser.APPROVE_OPTION) {
+        String filename = fileChooser.getSelectedFile().getPath();
+        if (!filename.endsWith(".txt")) {
+            filename += ".txt";
         }
-        try{
+        setTitle(filename);
+        try {
             FileWriter fileWriter = new FileWriter(filename);
             fileWriter.write(textArea.getText());
-            setTitle(filename);
             fileWriter.close();
-        }catch(IOException e){
-            
-            System.out.println("File Not Found");
+        } catch (IOException e) {
+            String errorMessage = "An error occurred while saving the file.";
+            JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+            System.out.print(errorMessage);
         }
-     
-     }
+    }
+}
      
      public void pasteItem(){
           try{
@@ -409,26 +504,33 @@ public class textEditorGUI extends javax.swing.JFrame {
         textArea.replaceRange("", textArea.getSelectionStart(), textArea.getSelectionEnd());
      }
      
-     public ImageIcon scaleImage(String location, int size){
-     BufferedImage img = null;
+     public ImageIcon scaleImage(String location, int size) {
+    BufferedImage img = null;
     try {
-    img = ImageIO.read(new File(location));
-     } catch (IOException e) {
+        img = ImageIO.read(new File(location));
+        int width = img.getWidth();
+        int height = img.getHeight();
+        int newWidth = size;
+        int newHeight = size;
+        if (width > height) {
+            newHeight = (int) (((double) size / width) * height);
+        } else {
+            newWidth = (int) (((double) size / height) * width);
+        }
+        Image dimg = img.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        return new ImageIcon(dimg);
+    } catch (IOException e) {
         e.printStackTrace();
-     }
-
-       
-        Image dimg = img.getScaledInstance(size, size,        Image.SCALE_SMOOTH);
-
-    ImageIcon p = new ImageIcon(dimg);
-        return p;
-     }
+        return null;
+    }
+}
      
      
      
      
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel btnClose;
     private javax.swing.JMenuItem itemClose;
     private javax.swing.JMenuItem itemCopy1;
     private javax.swing.JMenuItem itemCut1;
@@ -439,6 +541,7 @@ public class textEditorGUI extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPopupMenu mainMenu;
     private javax.swing.JPopupMenu subMenu;
